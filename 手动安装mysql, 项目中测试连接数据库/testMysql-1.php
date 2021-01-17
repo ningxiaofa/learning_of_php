60eb20f5c758469e0e383fec22c6f5cf06341d8c
william_ning <william_ning@qq.com>
2021-01-17 21:07:20 +0800

    测试PDO连接MySQL[含docker容器中的MySQL服务器]

--- 手动安装mysql, 项目中测试连接数据库/testMysql-1.php
+++ 手动安装mysql, 项目中测试连接数据库/testMysql-1.php
@@ -6,13 +6,15 @@ class Model extends PDO
     {
         $dbms = 'mysql';    //数据库类型
         $host = 'localhost';  //数据库主机名
+        $port = 33061; // 连接容器中的MySQL服务器, 但在 7.4下, 一直失败~~
         $dbName = 'test';     //使用的数据库
         $username = 'root';   //数据库连接用户名
-        $passwd = 'Nxf=2020'; //对应的密码
-        $dsn = "$dbms:host=$host;dbname=$dbName";
+        $passwd = 'Wn=2021.'; //对应的密码
+        $dsn = "$dbms:host=$host;port=$port;dbname=$dbName";
 		
         try{
             parent::__construct($dsn, $username, $passwd);
+            echo 'Success connected!';
         }catch(PDOException $exception){
            var_dump($exception->getMessage());
         }
@@ -20,12 +22,12 @@ class Model extends PDO
 }
 
 $model = new Model;
-$sql = "SELECT * FROM test";
-foreach ($model->query($sql) as $row) {
-	print $row['id'] . "\t";
-	print $row['title'] . "\t";
-	print $row['author'] . "\n";
-}
+// $sql = "SELECT * FROM test";
+// foreach ($model->query($sql) as $row) {
+// 	print $row['id'] . "\t";
+// 	print $row['title'] . "\t";
+// 	print $row['author'] . "\n";
+// }
 
 //输出结果：//数据表的数据只有： 1 test William
 //1 test William
