0b4efc90874895db469a67662824eb0a820de739
william ning <1158885641@qq.com>
2021-09-24 10:05:27 +0800

    验证 array_map 输入参数为空数组时，是否会报错 -- 不会报错

--- 数组函数/array_map/index.php
+++ 数组函数/array_map/index.php
@@ -18,3 +18,15 @@ $ret = array_map(function($value){
     return $value['fid'];
 }, $data);
 var_export($ret);
+
+// If empty array
+$members = [
+    [
+        'uid' => 'justtest'
+    ]
+];
+$members = []; // 并不会报错 输出： 空数组
+$guid_list = array_map(function($member){
+    return $member['uid'];
+}, $members);
+var_dump($guid_list);
