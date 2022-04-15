195aaca6b44ea7f02ac09b9c029a1680c858c4d5
William Ning <89905900+williamning114@users.noreply.github.com>
2022-04-15 17:01:29 +0800

    补充Socket说明

--- 支持的协议与封装协议/tcp-and-udp/index.php
+++ 支持的协议与封装协议/tcp-and-udp/index.php
@@ -2,3 +2,7 @@
 
 // https://www.php.cn/php-weizijiaocheng-392436.html -- PHP编程实现的TCP服务端和客户端的功能
 // 需要认真学习下
+// 这里的代码，服务端，只能处理单个客户端连接，并没有处理多个连接的问题，
+// 解决办法是多进程，多线程，epoll
+
+// https://time.geekbang.org/column/article/9293 [趣谈网络协议 || 第13讲 | 套接字Socket：Talk is cheap, show me the code]
