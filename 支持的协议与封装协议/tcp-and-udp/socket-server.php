70a1dad3bf664b52eacbb7a7d65274fe94c48b3e
william ning <1158885641@qq.com>
2022-12-06 11:38:06 +0800

    格式化代码风格

--- 支持的协议与封装协议/tcp-and-udp/socket-server.php
+++ 支持的协议与封装协议/tcp-and-udp/socket-server.php
@@ -10,7 +10,7 @@ $port = 3046;
  * 创建一个SOCKET
  * AF_INET=是ipv4 如果用ipv6，则参数为 AF_INET6
  * SOCK_STREAM为socket的tcp类型，如果是UDP则使用SOCK_DGRAM
-*/
+ */
 $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("socket_create() fail:" . socket_strerror(socket_last_error()) . "/n");
 
 //阻塞模式
@@ -27,14 +27,14 @@ echo "OK\nNow ready to accept connections.\nListening on the socket ... \n";
 do { // never stop the daemon
   //它接收连接请求并调用一个子连接Socket来处理客户端和服务器间的信息
   $msgsock = socket_accept($sock) or die("socket_accept() failed: reason: " . socket_strerror(socket_last_error()) . "/n");
-  while(1){
+  while (1) {
     //读取客户端数据
     echo "Read client data \n";
     //socket_read函数会一直读取客户端数据,直到遇见\n,\t或者\0字符.PHP脚本把这写字符看做是输入的结束符.
     $buf = socket_read($msgsock, 8192);
     echo "Received msg: $buf  \n";
 
-    if($buf == "bye"){
+    if ($buf == "bye") {
       //接收到结束消息，关闭连接，等待下一个连接
       socket_close($msgsock);
       continue;
@@ -42,9 +42,8 @@ do { // never stop the daemon
 
     //数据传送 向客户端写入返回结果
     $msg = "welcome \n";
-    socket_write($msgsock, $msg, strlen($msg)) or die("socket_write() failed: reason: " . socket_strerror(socket_last_error()) ."/n");
+    socket_write($msgsock, $msg, strlen($msg)) or die("socket_write() failed: reason: " . socket_strerror(socket_last_error()) . "/n");
   }
-
 } while (true);
 
 socket_close($sock);
